04c149dc288258540b0648ec147e392e74c627bd
roshankodi <roshankodi019@gmail.com>
2026-05-17 13:32:48 +0530

    Fixed Projects component

--- src/Components/Pill/Pill.jsx
+++ src/Components/Pill/Pill.jsx
@@ -1,75 +1,12 @@
-.pill{
+import React from 'react'
+import './Pill.css'
 
-display:flex;
-
-justify-content:center;
-
-align-items:center;
-
-width:fit-content;
-
-min-height:20px;
-
-padding:12px 18px;
-
-border:var(--border);
-
-border-radius:25px;
-
-margin:5px;
-
-color:var(--secondary);
-
-background-color:var(--primary);
-
-cursor:pointer;
-
-transition:0.3s ease-in-out;
-
-/* fixes long text */
-
-max-width:100%;
-
-white-space:normal;
-
-word-break:break-word;
-
-text-align:center;
-
+function Pill({content, color=""}) {
+  return (
+    <div className={`pill ${color}`}>
+      {content}
+    </div>
+  )
 }
 
-.pill:hover{
-
-color:var(--primary);
-
-background-color:var(--secondary);
-
-}
-
-.invert{
-
-color:var(--primary);
-
-background-color:var(--secondary);
-
-}
-
-.invert:hover{
-
-color:var(--secondary);
-
-background-color:var(--primary);
-
-}
-
-@media screen and (max-width:768px){
-
-.pill{
-
-font-size:0.8rem;
-
-padding:10px 14px;
-
-}
-
-}
+export default Pill
