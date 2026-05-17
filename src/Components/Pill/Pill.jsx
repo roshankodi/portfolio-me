880d89f9b6e510ac4e61bcf5632e95da663bb0cd
roshankodi <roshankodi019@gmail.com>
2026-05-17 13:22:45 +0530

    Fixed Projects component

--- src/Components/Pill/Pill.jsx
+++ src/Components/Pill/Pill.jsx
@@ -1,13 +1,75 @@
-import React from 'react'
-import '../../Global.css'
-import './Pill.css'
+.pill{
 
-function Pill({ content = 'Pill', color = 'default' }) {
-  return (
-    <div className={ color === 'default' ? "pill" : "pill invert" }>
-      {content}
-    </div>
-  )
+display:flex;
+
+justify-content:center;
+
+align-items:center;
+
+width:fit-content;
+
+min-height:20px;
+
+padding:12px 18px;
+
+border:var(--border);
+
+border-radius:25px;
+
+margin:5px;
+
+color:var(--secondary);
+
+background-color:var(--primary);
+
+cursor:pointer;
+
+transition:0.3s ease-in-out;
+
+/* fixes long text */
+
+max-width:100%;
+
+white-space:normal;
+
+word-break:break-word;
+
+text-align:center;
+
 }
 
-export default Pill
+.pill:hover{
+
+color:var(--primary);
+
+background-color:var(--secondary);
+
+}
+
+.invert{
+
+color:var(--primary);
+
+background-color:var(--secondary);
+
+}
+
+.invert:hover{
+
+color:var(--secondary);
+
+background-color:var(--primary);
+
+}
+
+@media screen and (max-width:768px){
+
+.pill{
+
+font-size:0.8rem;
+
+padding:10px 14px;
+
+}
+
+}
